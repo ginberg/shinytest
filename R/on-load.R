b0d2a9c1a6be11f427b4647b554ad973be130718
ginberg <ginberg@gmail.com>
2018-03-27 13:20:24 +0800

    check for phantomjs when library is loaded

--- R/on-load.R
+++ R/on-load.R
@@ -3,4 +3,17 @@
 
 .onLoad <- function(libname, pkgname) {
   debugme()
+
+  # Make sure phantomjs library is available..
+  # The executable should be available in the PATH. If it can't be found, it will be installed.
+  if (is.null(suppressMessages(shinytest:::find_phantom()))) {
+    webdriver::install_phantomjs()
+  }
+}
+
+.onAttach <- function(libname, pkgname) {
+  # Make sure phantomjs library is still available when the user attaches the package
+  if (is.null(suppressMessages(shinytest:::find_phantom()))) {
+    webdriver::install_phantomjs()
+  }
 }
